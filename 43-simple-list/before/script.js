7877508d797fc7cec93f2e3edd79b3f474c2459f
Meeran B <v9@live.co.uk>
2020-12-20 15:47:14 +0000

    simple-list: first completion

---
+++ 43-simple-list/before/script.js
@@ -0,0 +1,21 @@
+// Select all elements
+const form = document.querySelector("#new-item-form");
+const list = document.querySelector("#list");
+const input = document.querySelector("#item-input");
+// When form is submitted, add a new element
+
+form.addEventListener("submit", event => {
+  event.preventDefault();
+  if (!input.value) return;
+  const newToDoText = input.value;
+  const newListItem = document.createElement("div");
+  newListItem.innerText = newToDoText;
+  newListItem.classList.add("list-item");
+  list.appendChild(newListItem);
+  input.value = "";
+});
+
+list.addEventListener("click", event => {
+  const targetToDo = event.target;
+  list.removeChild(targetToDo);
+});
